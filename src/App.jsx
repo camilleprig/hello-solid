80180097df8739de6ca534b321becfe390902202
camille Prigent <camille.prigent37@gmail.com>
2024-02-16 16:06:49 +0100

    first commit

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,10 @@
 function App() {
   return (
-    <p class="text-4xl text-green-700 text-center py-20">Hello les Mines! J'essaie de m'amuser, ca marche</p>
+    <div>
+      <p class="text-4xl text-green-700 text-center py-20">Hello les Mines! J'essaie de m'amuser, ca marche</p>
+      <p class="text-4xl text-green-700 text-center py-20">Coucou</p>
+    </div>
+    
   );
 }
 
